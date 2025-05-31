348f6d09c7e1dc54aef6434cfcbbb4f57d134baf
Jnananjaya01 <jsamantarayjobs@gmail.com>
2025-05-31 09:35:59 +0530

    Add files via upload

---
+++ EnquiryRepo.java
@@ -0,0 +1,14 @@
+package com.sts.repos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.sts.entities.Enquiry;
+
+	public interface EnquiryRepo extends JpaRepository<Enquiry,Integer>{
+
+		@Query(value ="select * from enquiry_tbl where counsellor_id =:counsellorId",nativeQuery = true)
+		public List <Enquiry> getEnquriesByCounsellorId(Integer counsellorId);
+}
